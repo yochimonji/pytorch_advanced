7b49b77ad2a0ad77a5b33a97f0df76ca97aed9d7
yochimonji <yochimonji@gmail.com>
2022-04-15 15:46:21 +0900

    add Discriminatorを作成

--- pytorch_advanced/gan_generation/util/model.py
+++ pytorch_advanced/gan_generation/util/model.py
@@ -4,6 +4,12 @@ from torch import Tensor, nn
 
 
 class Generator(nn.Module):
+    layer1: nn.Sequential
+    layer2: nn.Sequential
+    layer3: nn.Sequential
+    layer4: nn.Sequential
+    last: nn.Sequential
+
     def __init__(self, z_dim: int = 20, image_size: int = 64):
         super().__init__()
 
@@ -37,6 +43,43 @@ class Generator(nn.Module):
 
     def forward(self, z: Tensor):
         out = self.layer1(z)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.last(out)
+        return cast(Tensor, out)
+
+
+class Discriminator(nn.Module):
+    layer1: nn.Sequential
+    layer2: nn.Sequential
+    layer3: nn.Sequential
+    layer4: nn.Sequential
+    last: nn.Conv2d
+
+    def __init__(self, image_size: int = 64):
+        super().__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=image_size, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=image_size, out_channels=image_size * 2, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=image_size * 2, out_channels=image_size * 4, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(in_channels=image_size * 4, out_channels=image_size * 8, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.last = nn.Conv2d(image_size * 8, 1, kernel_size=4, stride=1)
+
+    def forward(self, x: Tensor):
+        out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
--- tests/gan_generation/util/test_model.py
+++ tests/gan_generation/util/test_model.py
@@ -1,7 +1,7 @@
 from typing import cast
 
 import torch
-from pytorch_advanced.gan_generation.util.model import Generator
+from pytorch_advanced.gan_generation.util.model import Discriminator, Generator
 
 
 def test_generator():
@@ -9,3 +9,10 @@ def test_generator():
     input_z = torch.rand(1, 20, 1, 1)
     fake_image = cast(torch.Tensor, g(input_z))
     assert fake_image.size() == torch.Size([1, 1, 64, 64])
+
+
+def test_discriminator():
+    d = Discriminator(image_size=64)
+    image = torch.rand(1, 1, 64, 64)
+    out = cast(torch.Tensor, d(image))
+    assert out.size() == torch.Size([1, 1, 1, 1])
