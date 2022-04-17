ea4e295c7b1779836861beb35cdbd66960421f5d
yochimonji <yochimonji@gmail.com>
2022-04-17 14:10:04 +0900

    add SAGANを追加

--- pytorch_advanced/gan_generation/sagan.py
+++ pytorch_advanced/gan_generation/sagan.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import torch
-from torch import cuda, nn
+from torch import cuda
 from torch.utils.data import DataLoader
 
 from util.dataloader import GANImageDataset, ImageTransform, make_datapath_list
-from util.model import DCGANDiscriminator, DCGANGenerator, weights_init
-from util.train import train_dcgan
+from util.model import SAGANDiscriminator, SAGANGenerator, weights_init
+from util.train import train_sagan
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     std = (0.5,)
     batch_size = 64
     z_dim = 20
-    epochs = 200
+    epochs = 300
     torch.backends.cudnn.benchmark = True
 
     train_img_list = make_datapath_list("pytorch_advanced/gan_generation/data/img_78/")
@@ -21,8 +21,8 @@ def main():
     train_dataset = GANImageDataset(train_img_list, transform)
     train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=2, pin_memory=True)
 
-    generator = DCGANGenerator(z_dim=20, image_size=64)
-    discriminator = DCGANDiscriminator(image_size=64)
+    generator = SAGANGenerator(z_dim=20, image_size=64)
+    discriminator = SAGANDiscriminator(image_size=64)
     generator.apply(weights_init)
     discriminator.apply(weights_init)
     print("ネットワーク初期化完了")
@@ -34,22 +34,27 @@ def main():
     beta1, beta2 = 0.0, 0.9
     g_optimizer = torch.optim.Adam(generator.parameters(), g_lr, betas=(beta1, beta2))
     d_optimizer = torch.optim.Adam(discriminator.parameters(), d_lr, betas=(beta1, beta2))
-    criterion = nn.BCEWithLogitsLoss()
 
-    g, d = train_dcgan(
-        generator, discriminator, train_dataloader, g_optimizer, d_optimizer, criterion, z_dim, epochs, device=device
+    g, d = train_sagan(
+        generator, discriminator, train_dataloader, g_optimizer, d_optimizer, z_dim, epochs, device=device
     )
 
     fixed_z = torch.randn(batch_size, z_dim, 1, 1)
-    fake_images = g(fixed_z.to(device))
+    fake_images, am1, am2 = g(fixed_z.to(device))
     real_images = next(iter(train_dataloader))
 
-    _ = plt.figure(figsize=(15, 6))
+    _ = plt.figure(figsize=(15, 9))
     for i in range(0, 5):
-        plt.subplot(2, 5, i + 1)
+        plt.subplot(3, 5, i + 1)
         plt.imshow(real_images[i][0].cpu().detach().numpy(), "gray")
-        plt.subplot(2, 5, 5 + i + 1)
+
+        plt.subplot(3, 5, 5 + i + 1)
         plt.imshow(fake_images[i][0].cpu().detach().numpy(), "gray")
+
+        plt.subplot(3, 5, 10 + i + 1)
+        am = am1[i].view(16, 16, 16, 16)
+        am = am[7][7]
+        plt.imshow(am.cpu().detach().numpy(), "Reds")
     plt.show()
 
 
--- pytorch_advanced/gan_generation/util/train.py
+++ pytorch_advanced/gan_generation/util/train.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-def train_one_epoch(
+def train_dcgan_one_epoch(
     g: Module,
     d: Module,
     dataloader: DataLoader,
@@ -77,7 +77,7 @@ def train_one_epoch(
     return mean_g_loss, mean_d_loss
 
 
-def train_model(
+def train_dcgan(
     g: Module,
     d: Module,
     dataloader: DataLoader,
@@ -100,7 +100,103 @@ def train_model(
         print(f"Epoch {epoch+1}/{epochs}")
         print("---------------")
 
-        g_loss, d_loss = train_one_epoch(g, d, dataloader, g_optimizer, d_optimizer, criterion, z_dim, device)
+        g_loss, d_loss = train_dcgan_one_epoch(g, d, dataloader, g_optimizer, d_optimizer, criterion, z_dim, device)
+
+        t_epoch_finish = time.time()
+        print("---------------")
+        print(f"Epoch_G_Loss:{g_loss:.4f} ||Epoch_D_Loss:{d_loss:.4f} ||timer:{t_epoch_finish - t_epoch_start:.4f}")
+    return g, d
+
+
+def train_sagan_one_epoch(
+    g: Module,
+    d: Module,
+    dataloader: DataLoader,
+    g_optimizer: Optimizer,
+    d_optimizer: Optimizer,
+    z_dim=20,
+    device=torch.device("cpu"),
+) -> tuple[float, float]:
+    epoch_g_loss: float = 0.0
+    epoch_d_loss: float = 0.0
+
+    for images in tqdm(dataloader):
+        # Discriminatorの学習
+        images = cast(torch.Tensor, images)
+
+        if images.size(0) == 1:
+            continue
+
+        images = images.to(device)
+
+        mini_batch_size = images.size(0)
+
+        d_out_real, _, _ = d(images)
+        d_out_real = cast(torch.Tensor, d_out_real)
+
+        input_z = torch.randn(mini_batch_size, z_dim, 1, 1).to(device)
+        fake_images, _, _ = g(input_z)
+        d_out_fake, _, _ = d(fake_images)
+        d_out_fake = cast(torch.Tensor, d_out_fake)
+
+        d_loss_real = torch.nn.ReLU()(1.0 - d_out_real).mean()
+        d_loss_fake = torch.nn.ReLU()(1.0 + d_out_fake).mean()
+        d_loss = d_loss_real + d_loss_fake
+
+        g_optimizer.zero_grad()
+        d_optimizer.zero_grad()
+        d_loss.backward()
+        d_optimizer.step()
+
+        # Generatorの学習
+        input_z = torch.randn(mini_batch_size, z_dim, 1, 1).to(device)
+        fake_images, _, _ = g(input_z)
+        d_out_fake, _, _ = d(fake_images)
+        d_out_fake = cast(torch.Tensor, d_out_fake)
+
+        g_loss = -d_out_fake.mean()
+
+        g_optimizer.zero_grad()
+        d_optimizer.zero_grad()
+        g_loss.backward()
+        g_optimizer.step()
+
+        epoch_d_loss += cast(torch.Tensor, d_loss).cpu().item()
+        epoch_g_loss += cast(torch.Tensor, g_loss).cpu().item()
+
+    batch_size = dataloader.batch_size
+    if batch_size:
+        mean_g_loss = epoch_g_loss / batch_size
+        mean_d_loss = epoch_d_loss / batch_size
+    else:
+        mean_g_loss = epoch_g_loss
+        mean_d_loss = epoch_d_loss
+    return mean_g_loss, mean_d_loss
+
+
+def train_sagan(
+    g: Module,
+    d: Module,
+    dataloader: DataLoader,
+    g_optimizer: Optimizer,
+    d_optimizer: Optimizer,
+    z_dim=20,
+    epochs=200,
+    device=torch.device("cpu"),
+) -> tuple[Module, Module]:
+    g.to(device)
+    d.to(device)
+    g.train()
+    d.train()
+
+    for epoch in range(epochs):
+        t_epoch_start = time.time()
+
+        print("---------------")
+        print(f"Epoch {epoch+1}/{epochs}")
+        print("---------------")
+
+        g_loss, d_loss = train_sagan_one_epoch(g, d, dataloader, g_optimizer, d_optimizer, z_dim, device)
 
         t_epoch_finish = time.time()
         print("---------------")
